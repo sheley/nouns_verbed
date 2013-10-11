cfd3ec9bcf5fad36f71a64647b7ef24aee9a8c85
Michele Guido <micheleguido@gmail.com>
2013-10-11 16:25:33 +0200

    added form, request parameters are looked at and written to DB for tracked_things

--- nouns_verbed.rb
+++ nouns_verbed.rb
@@ -5,13 +5,38 @@ require 'sinatra'
 DB = Sequel.connect({:adapter => 'mysql2', :user => 'root', :host => 'localhost', :database => 'nouns_verbed'})
 
 get '/' do
-  '<html><body><h1>HALLO</h1></body></html>'
+  '<html>
+  <body>
+    <h1>HALLO</h1>
+    <form method="post" action="/new_entry">
+        <label for="noun_singular">Noun singular</label>
+        <input type="text" class="input" name="noun_singular"/>
+
+        <label for="noun_plural">Noun plural</label>
+        <input type="text" class="input" name="noun_plural"/>
+
+        <label for="verb_base">Verb base form</label>
+        <input type="text" class="input" name="verb_base"/>
+
+        <label for="verb_past">Verb past tense</label>
+        <input type="text" class="input" name="verb_past"/>
+
+
+        <input type="submit" value="Start Tracking">
+  </body>
+  </html>'
   # form!
 end
 
 post '/new_entry' do
-  # look at request parameters
-  # write to database
+  # look at request parameters and write to DB
+  DB[:tracked_things].insert_ignore << {
+    noun_singular:  params[:noun_singular],
+    noun_plural:    params[:noun_plural],
+    verb_base:      params[:verb_base],
+    verb_past:      params[:verb_past],
+  }
+    redirect '/'
   # respond with something.
 end
 
