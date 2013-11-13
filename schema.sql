ca73f51dc5d9f5ee5f69e5ecd9b4fdb0e8193774
Michele <micheleguido@gmail.com>
2013-11-13 14:53:01 +0100

    the database schema so far including -a users table -the tracked things table -the tracked data table
    -includes appropriate foreign key relationships between each

---
+++ schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE `tracked_data` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `count` int(11) NOT NULL,
+  `tracked_id` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_user_id` (`user_id`),
+  KEY `idx_tracked_id` (`tracked_id`),
+  CONSTRAINT `tracked_id` FOREIGN KEY (`tracked_id`) REFERENCES `tracked_things` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+CREATE TABLE `tracked_things` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `noun_singular` varchar(50) NOT NULL DEFAULT '',
+  `noun_plural` varchar(50) NOT NULL DEFAULT '',
+  `verb_base` varchar(50) NOT NULL DEFAULT '',
+  `verb_past` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL DEFAULT '',
+  `email` varchar(50) NOT NULL DEFAULT '',
+  `geox` float DEFAULT NULL,
+  `geoy` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
